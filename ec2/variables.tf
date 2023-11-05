a981ff82126921f93d3e36447633fed0005131ca
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-11-05 21:36:30 +0900

    Create variables.tf

---
+++ ec2/variables.tf
@@ -0,0 +1,19 @@
+variable "instance_ami" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "interface" {
+  type = string
+}
+
+variable "gw" {
+  type = string
+}
