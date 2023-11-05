4639c547a96a9c8474c63f1f98553799545f8a3a
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-11-05 21:33:33 +0900

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,25 @@
-output "bastion_ip"{
-	value = aws_instance.bastion.public_ip
+output "vpc_id" {
+  value = module.vpc.vpc_id
 }
+
+output "public_subnet_id_1" {
+  value = module.vpc.public_subnet_id_1
+}
+output "public_subnet_id_2" {
+  value = module.vpc.public_subnet_id_2
+}
+output "public_subnet_id_3" {
+  value = module.vpc.public_subnet_id_3
+}
+
+output "private_subnet_id_1" {
+  value = module.vpc.private_subnet_id_1
+}
+output "private_subnet_id_2" {
+  value = module.vpc.private_subnet_id_2
+}
+
+output "private_subnet_id_3" {
+  value = module.vpc.private_subnet_id_3
+}
+
