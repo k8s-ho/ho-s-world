8a8feb732f818f93ea87c77ade85d5239e449bc3
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-11-05 21:35:46 +0900

    Create variables.tf

---
+++ vpc/variables.tf
@@ -0,0 +1,31 @@
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_cidr_1" {
+  type = string
+}
+
+variable "public_subnet_cidr_2" {
+  type = string
+}
+
+variable "public_subnet_cidr_3" {
+  type = string
+}
+
+variable "private_subnet_cidr_1" {
+  type = string
+}
+
+variable "private_subnet_cidr_2" {
+  type = string
+}
+
+variable "private_subnet_cidr_3" {
+  type = string
+}
+
+variable "bastion_private_ip" {
+  type = string
+}
