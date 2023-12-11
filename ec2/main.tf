a7983cf6dadd3fd14a608cc04da09f80bdd5d0e6
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-12-11 21:15:04 +0900

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,22 +1,21 @@
 resource "aws_instance" "server" {
-	ami				= var.instance_ami
-	instance_type   = var.instance_type
-	key_name = var.key_name
-	subnet_id = var.subnet
+  ami  = var.instance_ami
+  instance_type = var.instance_type
+  key_name = var.key_name
+  subnet_id = var.subnet
   associate_public_ip_address = false
   vpc_security_group_ids = var.sg
 
-	user_data = <<-EOT
-		#!/bin/bash
-		sudo apt update && sudo apt install unzip
-		sudo apt install net-tools && apt install -y jq
+  user_data = <<-EOT
+  #!/bin/bash
+  sudo apt update && sudo apt install unzip
+  sudo apt install net-tools && apt install -y jq
 
-		echo "sudo su -" >> /home/ubuntu/.bashrc
-		sudo hostnamectl --static set-hostname ${var.hostname}
+  echo "sudo su -" >> /home/ubuntu/.bashrc
+  sudo hostnamectl --static set-hostname ${var.hostname}
+  EOT
 
-		EOT
-  
-	tags = {
-	  Name = var.hostname
-	}
+  tags = {
+    Name = var.hostname
+  }
 }
