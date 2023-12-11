46c728c699df01f726b96b3dfff0b489a8692f54
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-12-11 17:58:20 +0900

    Create variables.tf

---
+++ bastion/variables.tf
@@ -0,0 +1,21 @@
+variable "instance_ami" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "interface" {
+  type = string
+}
+
+variable "gw" {
+  type = string
+}
+
+
