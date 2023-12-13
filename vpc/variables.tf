c41612bb12f7a24032794efb11a4b8c65db795be
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-12-13 17:23:27 +0900

    Update variables.tf

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -2,28 +2,14 @@ variable "vpc_cidr" {
   type = string
 }
 
-variable "public_subnet_cidr_1" {
-  type = string
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
 }
 
-variable "public_subnet_cidr_2" {
-  type = string
-}
-
-variable "public_subnet_cidr_3" {
-  type = string
-}
-
-variable "private_subnet_cidr_1" {
-  type = string
-}
-
-variable "private_subnet_cidr_2" {
-  type = string
-}
-
-variable "private_subnet_cidr_3" {
-  type = string
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["192.168.11.0/24", "192.168.12.0/24", "192.168.13.0/24"]
 }
 
 variable "bastion_private_ip" {
