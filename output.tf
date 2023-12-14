f2bfd3a5bdafb770cecd2753dc7d2ebb4d47a9f1
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-12-14 16:40:09 +0900

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,35 +1,11 @@
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-
-output "public_subnet_id_1" {
-  value = module.vpc.public_subnet_id_1
-}
-
-output "public_subnet_id_2" {
-  value = module.vpc.public_subnet_id_2
-}
-
-output "public_subnet_id_3" {
-  value = module.vpc.public_subnet_id_3
-}
-
-output "private_subnet_id_1" {
-  value = module.vpc.private_subnet_id_1
-}
-
-output "private_subnet_id_2" {
-  value = module.vpc.private_subnet_id_2
-}
-
-output "private_subnet_id_3" {
-  value = module.vpc.private_subnet_id_3
-}
-
 output "bastion_public_ip" {
   value = module.bastion.bastion_ip
 }
 
 output "private_instance_ip" {
   value = module.server[*].server_ip
+}
+
+output "application_loadBalance_domain" {
+  value = module.route53.domain
 }
