21e9f65df6e1d99c550464144d195a103d29c66f
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-12-14 16:45:04 +0900

    Update variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -10,14 +10,14 @@ variable "hostname" {
   type = string
 }
 
-variable "subnet" {
-  type = string
-}
-
 variable "sg" {
   type = list(string)
 }
 
 variable "key_name" {
+  type = string
+}
+
+variable "subnet" {
   type = string
 }
