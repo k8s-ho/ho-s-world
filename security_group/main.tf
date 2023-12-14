56038603517816268fd780d947e311d7ae69c943
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-12-14 16:44:36 +0900

    Update main.tf

--- security_group/main.tf
+++ security_group/main.tf
@@ -37,6 +37,13 @@ resource "aws_security_group" "private_ec2"{
     cidr_blocks = [var.bastion_private_ip] # 나중에 삭제ㄱ
   }
 
+  ingress {
+    from_port   = 7777
+    to_port     = 7777
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -49,13 +56,33 @@ resource "aws_security_group" "private_ec2"{
   }
 }
 
-/*
-resource "aws_security_group_rule" "private_ec2" {
-  type                     = "ingress"
-  from_port                = 22
-  to_port                  = 22
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.private_ec2.id
-  source_security_group_id = aws_security_group.allow_bastion.id
+# LoadBalancer SG
+resource "aws_security_group" "loadbalancer_sg"{
+    name        = "loadbalancer-sg"
+    vpc_id      =  var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "loadbalancer-sg"
+  }
 }
-*/
