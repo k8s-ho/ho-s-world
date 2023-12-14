bdaedbc93bc6095f1b281de817bc0060482e05df
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-12-14 16:47:07 +0900

    Create variables.tf

---
+++ route53/variables.tf
@@ -0,0 +1,15 @@
+variable "hostzone_id" {
+  type = string
+}
+
+variable "record_name" {
+  type = string
+}
+
+variable "lb_zone_id" {
+  type = string
+}
+
+variable "lb_dns_name" {
+  type = string
+}
