6f0254e04d48b4442f6b46b4d533345a5330e3ac
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-12-17 21:22:52 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,11 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-variable "key_name" {
-  description = "SSH key name"
-  default     = "abc" # < Enter your key pair name >
+locals {
+  key_name = "abc" # < Enter your key pair name >
+  certificate = "arn:aws:acm:ap-northeast-2:123412341234:certificate/12345678-1234-abcd-abcd-234523452345" # <Enter your ssl certificate arn>
+  route53_hostzoneid = "000000000000000000000"  # <Enter your route53_hostzone id>
+  route53_record_name = "my-world" # <Enter your route53 record name >
 }
 
 module "security_group"{
@@ -21,7 +23,7 @@ module "vpc" {
 
   # LoadBalancing config
   lb_sg = [module.security_group.lb_sg]
-  certificate = "arn:aws:acm:ap-northeast-2:123412341234:certificate/12345678-1234-abcd-abcd-234523452345" # <your_ssl_certificate_arn>
+  certificate =  local.certificate
   lb_attach_ec2_ids  = flatten([for instance in module.server : instance.server_ec2_ids])
 }
 
@@ -29,7 +31,7 @@ module "bastion" {
   source = "./bastion"
   instance_ami = "ami-09eb4311cbaecf89d"
   instance_type = "t3.medium"
-  key_name = var.key_name
+  key_name = local.key_name
   interface = module.vpc.ec2_interface # <- include sg
   gw = module.vpc.internet_gw
 }
@@ -38,7 +40,7 @@ module "server" {
   source = "./ec2"
   instance_ami = "ami-09eb4311cbaecf89d"
   instance_type = "t3.medium"
-  key_name = var.key_name
+  key_name = local.key_name
   count = 2
   hostname = "private-${count.index}"
   subnet = module.vpc.private_subnet_ids[count.index]
@@ -47,8 +49,8 @@ module "server" {
 
 module "route53" {
   source = "./route53"
-  hostzone_id = "000000000000000000000" # <Your route53 hostzone ID>
-  record_name = "my-world"  # <Route 53 record name you want>
+  hostzone_id = local.route53_hostzoneid
+  record_name = local.route53_record_name
   lb_dns_name = module.vpc.lb_dns_name
   lb_zone_id = module.vpc.lb_zone_id
 }
@@ -58,7 +60,7 @@ resource "null_resource" "copy_ssh_key_bastion" {
   provisioner "local-exec" { # 혹시몰라서 인스턴스 생성 30초 대기 후 명령 진
     command = <<-EOT
       sleep 30
-      scp -o StrictHostKeyChecking=no -i ${var.key_name}.pem ${var.key_name}.pem ubuntu@${module.bastion.bastion_ip}:.ssh/id_rsa
+      scp -o StrictHostKeyChecking=no -i ${local.key_name}.pem ${local.key_name}.pem ubuntu@${module.bastion.bastion_ip}:.ssh/id_rsa
     EOT
   }
 }
