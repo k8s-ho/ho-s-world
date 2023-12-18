84de6ed528f1712d87f2f41872c535e7fc158772
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-12-19 00:05:34 +0900

    Update output.tf

--- output.tf
+++ output.tf
@@ -9,3 +9,8 @@ output "private_instance_ip" {
 output "application_loadBalance_domain" {
   value = module.route53.domain
 }
+
+output "eks_endpoint" {
+  description = "Endpoint for your Kubernetes API server"
+  value       = module.eks.cluster_endpoint
+}
